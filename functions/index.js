d0d5386c84268641808512fbe71bdcc52d483688
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-01 16:09:57 -0700

    set up firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,14 @@
-var functions = require('firebase-functions');
-
+const functions = require('firebase-functions');
+const createUser = require('./create_user');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+exports.goodbye = functions.https.onRequest((request, response) => {
+  response.send("Goodbye!");
+});
+
+exports.createUser = functions.https.onRequest(createUser);
