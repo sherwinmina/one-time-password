56a0a80a97e5d2d76995892c36ff4d17893cbcbf
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-01 16:44:42 -0700

    set up sirebase admin

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,11 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+const serviceAccount = require('./service_account.json')
 
-exports.goodbye = functions.https.onRequest((request, response) => {
-  response.send("Goodbye!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://one-time-password-a425c.firebaseio.com"
 });
 
 exports.createUser = functions.https.onRequest(createUser);
