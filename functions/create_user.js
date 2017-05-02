3efd992eccd75f47709a397733a97f2c3c360c07
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-01 18:47:37 -0700

    create new user with phone number

--- functions/create_user.js
+++ functions/create_user.js
@@ -6,8 +6,10 @@ module.exports = function(req, res) {
     return res.status(422).send({ error: 'Bad Input' });
   }
   // Format phone number to remove dashes and parens
-  const phone = String(req.body.phone);
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
   // create a new user account using the phone number
-
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }))
   // Respond to the user request, saying the account was made
 }
