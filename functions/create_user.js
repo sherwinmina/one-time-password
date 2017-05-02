a0501fd36c328caa2b6db1612038286e4ffea0a6
Sherwin Mina <sherwinmina@yahoo.com>
2017-05-01 18:20:06 -0700

    verify phone number

--- functions/create_user.js
+++ functions/create_user.js
@@ -1,3 +1,13 @@
+const admin = require('firebase-admin');
+
 module.exports = function(req, res) {
-  res.send(req.body);
+  // Verify the user provided a phone
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+  // Format phone number to remove dashes and parens
+  const phone = String(req.body.phone);
+  // create a new user account using the phone number
+
+  // Respond to the user request, saying the account was made
 }
